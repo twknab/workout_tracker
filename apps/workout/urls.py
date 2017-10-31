c9a981cf789aeba49f8c574d5b6490fed233b7f5
twknab <20636750+twknab@users.noreply.github.com>
2017-10-30 19:43:31 -0700

    Added in bootstrap and a bootstrap dark theme. Customized them and organized static files.

--- apps/workout/urls.py
+++ apps/workout/urls.py
@@ -6,5 +6,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index),
+    url(r'^$', views.login),
+    url(r'^register$', views.register),
+    url(r'^dashboard$', views.dashboard),
+    url(r'^tables$', views.tables),
+    url(r'^charts$', views.charts),
+    url(r'^forms$', views.forms),
 ]
--- apps/workout/views.py
+++ apps/workout/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render, redirect
 
-# Create your views here.
-def index(request):
-    """Loads homepage."""
+def login(request):
+    """Loads login."""
 
-    return render(request, "workout/index.html")
+    if request.method == "GET":
+        return render(request, "workout/index.html")
+
+def register(request):
+    """Register a user."""
+
+    if request.method == "GET":
+        return render(request, "workout/register.html")
+
+def dashboard(request):
+    """Loads dashboard."""
+
+    return render(request, "workout/dashboard.html")
+
+def tables(request):
+    """Loads tables."""
+
+    return render(request, "workout/tables.html")
+
+def charts(request):
+    """Loads charts."""
+
+    return render(request, "workout/charts.html")
+
+def forms(request):
+    """Loads forms."""
+
+    return render(request, "workout/forms.html")
